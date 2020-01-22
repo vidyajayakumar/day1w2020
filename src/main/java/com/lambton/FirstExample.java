867a1b66ab3d1e6154213c0856af898ea470b0b1
MacStudent <macstudent@128-IMAC34.local>
2020-01-21 20:51:18 -0500

    [vidya] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,16 @@
+package com.lambton;
+
+public class FirstExample
+{
+    public static void main(String[] args)
+    {
+        System.out. println("Welcome to java");
+
+        for(int i =1;i <=10; i++)
+        {
+            System.out.println("Vidya");
+        }
+
+
+    }
+}
